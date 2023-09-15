35614549497bb3767f28cab6459fffac20aa8032
Prakash Chand <Prakash0719pc@gmail.com>
2023-09-15 13:43:22 +0530

    update

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,64 +1,54 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Carousel() {
-
   const [getGame, setGame] = useState([]);
   useEffect(() => {
     getGameDetails();
-    console.log(getGame);
   }, []);
 
   const getGameDetails = async () => {
     const res = await axios.get("/api/getAllSports");
     console.log(res.data);
-    setGame(res.data);
     checkFilter(res.data);
-    console.log(getGame);
   };
-    const checkFilter= (gameName:any)=>{
-        const filterData = gameName.filter((game:any) => game.isFeatured ==true);
-        setGame(filterData);
-        console.log(filterData,'saodiafdas');
-        console.log(getGame,'prakas');
-    }
-
+  const checkFilter = (gameName: any) => {
+    const filterData = gameName.filter((game: any) => game.isFeatured == true);
+    setGame(filterData);
+    console.log(getGame, "prakas");
+    console.log(filterData, "saodiafdas");
+  };
 
   return (
-   <>
-<div className="carousel w-full h-1/2">
-  <div id="slide1" className="carousel-item relative w-full h-1/2">
-    <img src="https://images-eu.ssl-images-amazon.com/images/G/31/OHL/23/Central/BAU/Unrec/21to25/PC_hero_1_2x._CB598561652_.jpg" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-    <button className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">serach</button>
-    {/* {getGame} */}
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Beauty/Makeup-PChj._CB577919774_.jpg" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-</div>
-   </>
+    <>
+      <div className="carousel w-full h-[35vh]">
+        {getGame.map((game?:any , index?:any) => {
+          console.log('hii');
+          return (
+            <>
+              <div id={`slide`+ index} className="carousel-item relative w-full h-full">
+                <img
+                  src="https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/4529fd434a85c683.jpg?q=20"
+                  className="w-full "
+                />
+                
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href={`#slide`+ eval("index - 1")} className="btn btn-circle">
+                    ❮
+                  </a>
+                  <a href={`#slide`+ eval(index +1)} className="btn btn-circle">
+                    ❯
+                  </a>
+                </div>
+                <button className="btn btn-primary absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-3/4 mx-auto w-3/12 md:w-2/12">
+                  Register Now
+                </button>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 }
---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <>
+      <div className="flex justify-center mt-16">
+        <span className="loading loading-bars loading-lg mt-8"></span>
+      </div>
+    </>
+  );
+}

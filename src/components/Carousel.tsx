7e88076465626de738b4354e8a1b253e4866a6b3
Prakash Chand <Prakash0719pc@gmail.com>
2023-09-17 13:49:56 +0530

    update

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -37,10 +37,16 @@ export default function Carousel() {
                 />
 
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                  <a href={`#slide${index - 1}`} className="btn btn-circle">
+                  <a
+                    href={`#slide` + eval("index - 1")}
+                    className="btn btn-circle"
+                  >
                     ❮
                   </a>
-                  <a href={`#slide${index + 1}`} className="btn btn-circle">
+                  <a
+                    href={`#slide` + eval(index + 1)}
+                    className="btn btn-circle"
+                  >
                     ❯
                   </a>
                 </div>

00042572462c13a02ba0d6ee41836eae75089636
Rishabh Jha <rishabhjha0@gmail.com>
2023-09-26 12:59:28 +0530

    changes ui

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -53,82 +53,39 @@ export default function Card() {
   } else {
     return (
       <>
-          <div className="flex justify-center items-center my-6 ">
-          <h1 className="md:text-4xl text-2xl border-b-2 border-sky-500 font-serif">LetzKhelo Competetions</h1>
-       </div>
-        
+        <div className="flex justify-center items-center my-6 ">
+          <h1 className="md:text-4xl text-2xl border-b-2 border-sky-500 font-serif">
+            LetzKhelo Competetions
+          </h1>
+        </div>
+
         <div className="p-4 w-full md:flex md:justify-evenly md:flex-wrap">
           {getGame?.map((game: any, id) => {
             return (
-              <>
-                {/* <div
-                className=" md:w-1/4 max-w-md  bg-white rounded-3xl shadow-xl overflow-hidden m-8"
-                key={id}
-              >
-                <div className="max-w-md mx-auto">
-                  <div className="h-[236px]">
-                    <Image
-                      src={game?.image}
-                      alt="img"
-                      width="300"
-                      height="200"
-                      className="rounded-t-lg object-cover h-[100%] w-[100%]"
-                    />
-                  </div>
-                  <div className="p-4 sm:p-6">
-                    <p className="font-bold text-gray-700 text-[22px] leading-7 mb-1">
-                      {game?.sportName}
-                    </p>
-                    <div className="flex flex-row">
-                      <p className="text-[17px] font-bold text-[#0FB478]">
-                        location: {game?.location}
-                      </p>
-                    </div>
-                    <div className="flex flex-row">
-                      <p className="text-[17px] font-bold text-[#0FB478]">
-                        Registered Users: {countRegisteredUsers(game?.sportName)}
-                      </p>
-                    </div>
-                    <button className="block mt-10 w-full px-4 py-3 font-medium tracking-wide text-center capitalize transition-colors duration-300 transform bg-[#FFC933] rounded-[14px] hover:bg-[#FFC933DD] focus:outline-none focus:ring focus:ring-teal-300 focus:ring-opacity-80">
+              <div key={id} className="card w-80 p-2 glass transition-transform transform hover:scale-105 duration-300">
+                <figure>
+                  <Image
+                    src={game?.image}
+                    alt="img"
+                    width="300"
+                    height="200"
+                    className="rounded-t-lg object-cover h-[100%] w-[100%]"
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{game?.sportName}</h2>
+                  <p>location: {game?.location}</p>
+                  <p>Participants: {countRegisteredUsers(game?.sportName)}</p>
+                  <div className="card-actions justify-end">
+                    <button className="btn btn-primary transform transition-transform hover:scale-105 duration-300">
+                      {" "}
                       <Link href={`/bookCompetetion/${game?._id}`}>
                         Register Now
                       </Link>
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9"
-                      />
                     </button>
                   </div>
                 </div>
-              </div> */}
-                <div className="card w-80 p-2 glass">
-                  <figure>
-                    <Image
-                      src={game?.image}
-                      alt="img"
-                      width="300"
-                      height="200"
-                      className="rounded-t-lg object-cover h-[100%] w-[100%]"
-                    />
-                  </figure>
-                  <div className="card-body">
-                    <h2 className="card-title">{game?.sportName}</h2>
-                    <p>location: {game?.location}</p>
-                    <p>Participants: {countRegisteredUsers(game?.sportName)}</p>
-                    <div className="card-actions justify-end">
-                      <button className="btn btn-primary">
-                        {" "}
-                        <Link href={`/bookCompetetion/${game?._id}`}>
-                          Register Now
-                        </Link>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </>
+              </div>
             );
           })}
         </div>
--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,31 +1,30 @@
 "use client"
-
-import React from 'react'
-import { useState } from 'react'
-import Carousel from './Carousel'
-import Card from './Card'
+import React, { useState, useEffect } from 'react';
+import Carousel from './Carousel';
+import Card from './Card';
 
 export const HomeComponent = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   return (
     <div>
       {/* <Carousel/> */}
       <section className="background firstsection">
         <div className="box-main">
-            <div className="fhalf flex flex-col p-20" id="home">
-                <p className="textbig ">The Future is here</p>
-                <p className="textsmall ">Come out of the <b>virtual world</b></p>
-
-            </div>
-            <div className="shalf">
-                <img src='/icon.jpeg' alt="kk"/>
-              
-
-            </div>
-            
+          <div className={`fhalf flex flex-col p-20 ${isMounted ? 'animate-fadeInLeft' : ''}`} id="home">
+            <p className={`textbig ${isMounted ? 'animate-bounce' : ''}`}>The Future is here</p>
+            <p className={`textsmall ${isMounted ? 'animate-fadeInRight' : ''}`}>Come out of the <b>virtual world</b></p>
+          </div>
+          <div className={`shalf ${isMounted ? 'animate-zoomIn' : ''}`}>
+            <img src='/icon.jpeg' alt="kk" />
+          </div>
         </div>
-    </section>
-      <Card/> 
+      </section>
+      <Card />
     </div>
-  )
-}
+  );
+};

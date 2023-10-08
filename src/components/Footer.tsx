17419f0a38dc8a8367d80bc837e471f93db4a7c0
Prakash Chand <Prakash0719pc@gmail.com>
2023-10-08 23:34:21 +0530

    privacyPolicy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
         <nav>
           <header className="footer-title text-black">Legal</header>
           <Link href={'/'} className="link link-hover text-black">Terms of use</Link>
-          <Link href={'/'} className="link link-hover text-black">Privacy policy</Link>
+          <Link href={'/privacyPolicy'} className="link link-hover text-black">Privacy policy</Link>
           <Link href={'/'} className="link link-hover text-black">Cookie policy</Link>
         </nav>
         <nav>

c7e7ae44ad8c83b0705fefd17712b69fa2b143c9
letzkhelo <letzkhello@gmail.com>
2023-10-12 19:22:31 +0530

    update

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -9,9 +9,9 @@ export const HomeComponent = () => {
 
   const slides = [
     { url: "https://www.letzkhelo.com//image-1.jpg", title: "beach" },
-    { url: "http://localhost:3000/image-2.jpg", title: "boat" },
-    { url: "http://localhost:3000/image-3.jpg", title: "forest" },
-    { url: "http://localhost:3000/image-4.jpg", title: "city" },
+    { url: "https://www.letzkhelo.com//image-2.jpg", title: "boat" },
+    { url: "https://www.letzkhelo.com//image-3.jpg", title: "forest" },
+    { url: "https://www.letzkhelo.com//image-4.jpg", title: "city" },
   ];
 
   useEffect(() => {

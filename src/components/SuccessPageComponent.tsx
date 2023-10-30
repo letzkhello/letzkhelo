777ee382699900459c86232b0eb9a2402825d1ce
letzkhelo <letzkhello@gmail.com>
2023-10-30 15:32:21 +0530

    update

--- src/components/SuccessPageComponent.tsx
+++ src/components/SuccessPageComponent.tsx
@@ -29,7 +29,7 @@ export const SuccessPageComponent = ({ params }: any) => {
 
   return (
     <div className="h-screen">
-      <div className="flex flex-col items-center justify-center h-4/6">
+      <div className="flex flex-col items-center justify-center h-4/6 mt-16 sm:mt-4 md:mt-3 lg:mt-3">
         <div className="flex flex-col items-center justify-center ">
           <div className="w-20 h-20 bg-green-500 rounded-full flex items-center justify-center">
             <div className="text-2xl h-full w-full flex justify-center items-center">
@@ -54,7 +54,7 @@ export const SuccessPageComponent = ({ params }: any) => {
             }
           })}
         </p>
-        <p className="text-lg text-white">
+        <p className="text-lg text-white px-10 mt-8">
           <span className="font-bold uppercase">Venue:</span>
           {getGame?.map((game) => {
             if (game?._id == params?.id) {
@@ -67,15 +67,24 @@ export const SuccessPageComponent = ({ params }: any) => {
           })}
         </p>
 
-        <Link href={"/"}>
+        <p className="text-lg text-white mt-8 px-10">
+        Please remember to bring your Aadhar card for verification purposes. It helps us ensure accurate and secure identification.
+        </p>
+
+        <Link href={"/"} className="my-4 p-0">
           <button
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center lg:text-xl text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 hover:scale-110 duration-300
-    my-12"
+            className="inline-flex items-center px-3 py-2 mb-4 text-sm font-medium text-center lg:text-xl text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 hover:scale-110 duration-300"
           >
             Go Back to Home
           </button>
         </Link>
       </div>
+
+      <div className="flex justify-center items-center mt-4">
+           <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d7004.169407169525!2d77.02187234330985!3d28.62722335267022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjjCsDM3JzM1LjQiTiA3N8KwMDEnMjMuOCJF!5e0!3m2!1sen!2sin!4v1698658857499!5m2!1sen!2sin" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="border-0 w-3/4 h-44 sm:w-3/4 sm:h-44"></iframe>
+           </div>
     </div>
   );
 };
+
+

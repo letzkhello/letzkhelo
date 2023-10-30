8f9d2a7a378ce38b19c68b41ec1b2c411d8f33c8
letzkhelo <letzkhello@gmail.com>
2023-10-30 16:06:49 +0530

    update

--- src/components/SuccessPageComponent.tsx
+++ src/components/SuccessPageComponent.tsx
@@ -29,7 +29,7 @@ export const SuccessPageComponent = ({ params }: any) => {
 
   return (
     <div className="h-screen">
-      <div className="flex flex-col items-center justify-center h-4/6 mt-16 sm:mt-4 md:mt-3 lg:mt-3">
+      <div className="flex flex-col items-center justify-center h-4/6 my-16 sm:my-4 md:my-3 lg:my-3">
         <div className="flex flex-col items-center justify-center ">
           <div className="w-20 h-20 bg-green-500 rounded-full flex items-center justify-center">
             <div className="text-2xl h-full w-full flex justify-center items-center">
@@ -38,7 +38,7 @@ export const SuccessPageComponent = ({ params }: any) => {
           </div>
         </div>
 
-        <h1 className="text-3xl font-bold text-green-600 mb-4">
+        <h1 className="text-3xl font-bold text-green-600 my-4">
           Registration Successful
         </h1>
         <p className="text-lg text-white px-10">
@@ -54,7 +54,7 @@ export const SuccessPageComponent = ({ params }: any) => {
             }
           })}
         </p>
-        <p className="text-lg text-white px-10 mt-8">
+        <p className="text-lg text-white px-10 my-4">
           <span className="font-bold uppercase">Venue : </span>
           {getGame?.map((game) => {
             if (game?._id == params?.id) {
@@ -67,12 +67,12 @@ export const SuccessPageComponent = ({ params }: any) => {
           })}
         </p>
 
-        <p className="text-lg text-white px-10 mt-8">
+        <p className="text-lg text-white px-10 my-4">
           <span className="font-bold uppercase">Time : </span>
             11:00 AM
         </p>
 
-        <p className="text-lg text-white mt-8 px-10">
+        <p className="text-lg text-white my-4 px-10">
         Please remember to bring your Aadhar card for verification purposes. It helps us ensure accurate and secure identification.
         </p>
 
@@ -85,7 +85,7 @@ export const SuccessPageComponent = ({ params }: any) => {
         </Link>
       </div>
 
-      <div className="flex justify-center items-center mt-4">
+      <div className="flex justify-center items-center my-4">
            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d7004.169407169525!2d77.02187234330985!3d28.62722335267022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjjCsDM3JzM1LjQiTiA3N8KwMDEnMjMuOCJF!5e0!3m2!1sen!2sin!4v1698658857499!5m2!1sen!2sin" width="600" height="450" loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="border-0 w-3/4 h-44 sm:w-3/4 sm:h-44"></iframe>
            </div>
     </div>

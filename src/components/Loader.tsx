47344ff85f84084ccfb3c34dbf0df5e5defa0ca8
Prakash Chand <Prakash0719pc@gmail.com>
2023-11-02 23:18:45 +0530

    footer_update

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,6 +3,7 @@ export default function Loader() {
     <>
       <div className="h-3/4 w-full flex justify-center items-center mt-10">
         <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-white"></div>
+        
       </div>
     </>
   );

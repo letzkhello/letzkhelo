49a211f07ff0eacfee057d9d7f36e8dd1e21e1b7
rishabhjha12-hub <76894646+rishabhjha12-hub@users.noreply.github.com>
2023-11-08 23:09:17 +0530

    fixed carousel

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import Carousel from "./Carousel";
 import Card from "./Card";
 import Features from "./OurFeatures";
+import Image from "next/image";
+
 
 export const HomeComponent = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -20,9 +22,19 @@ export const HomeComponent = () => {
 
   return (
     <div>
-      <div className="w-500 h-280 m-0 auto mb-10">
-        <Carousel slides={slides} />
-      </div>
+      {/* <div className="w-500 h-280 m-0 auto mb-10 "> */}
+        {/* <Carousel slides={slides} /> */}
+        {/* <Image
+            src="/group.jpg"
+            layout='fill'
+    objectFit='contain'
+            alt="Google Icon"
+            className="mr-3"
+          /> */}
+          <p className="absolute top-40 z-50 text-orange-600 text-4xl font-black p-2 animate-waving-hand animate-pulse text-center">Ab Har Gali Se Champion Niklenge</p>
+          
+          <img className="h-96 w-[100%] opacity-80" src="/group.jpg"/>
+      {/* </div> */}
 
       <Card />
       <div className="m-0 p-0">

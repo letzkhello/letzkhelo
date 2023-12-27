3600984f6908593922cab3f23057f4df8487726e
rishabhjha12-hub <76894646+rishabhjha12-hub@users.noreply.github.com>
2023-12-27 22:22:03 +0530

    add product api

---
+++ src/models/product.ts
@@ -0,0 +1,62 @@
+
+
+import mongoose, { Schema, Model, Document } from "mongoose";
+
+interface IProduct extends Document {
+  productName: string;
+  category: string;
+  description?: string;
+  expectedDelievery?: Date;
+  isFeatured: boolean;
+  inStock: boolean;
+  price: Number;
+  discountedPrice:Number;
+  imageLink: string;
+}
+
+const ProductSchema = new Schema<IProduct>({
+    productName: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  category: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  description: {
+    type: Schema.Types.String,
+    required: false,
+  },
+  
+  expectedDelievery: {
+    type: Schema.Types.Date,
+    required: true,
+  },
+  isFeatured: {
+    type: Schema.Types.Boolean,
+    default: false,
+  },
+  inStock: {
+    type: Schema.Types.Boolean,
+    default: false,
+    required: false,
+  },
+  
+  price: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+  discountedPrice: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+  imageLink: {
+    type: Schema.Types.String,
+    required: false,
+  },
+});
+
+const Addproduct: Model<IProduct> =
+  mongoose.models.Addproduct || mongoose.model<IProduct>("Addproduct", ProductSchema);
+
+export default Addproduct;

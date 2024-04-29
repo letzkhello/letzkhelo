2c1b7158635930e31186c9f53f7afdb572fb8f2d
abhi0504 <sharmaash876@gmail.com>
2024-04-29 17:25:08 +0530

    new commit

--- src/app/api/dispatchMails/route.tsx
+++ src/app/api/dispatchMails/route.tsx
@@ -14,13 +14,13 @@ async function sendEmail(user:any) {
       <h1 style="color: #333;">🎉 Get Ready for an Exciting Arm Wrestling Event - <b>Letzkhelo</b> is Back! 🎉</h1>  
       <p>Dear <b>${user.name}</b>,</p>  
       <p>Greetings to all our enthusiastic and passionate arm wrestling contenders! We hope this email finds you in high spirits and ready to flex those muscles because we have some thrilling news to share.</p>  
-      <p>We are thrilled to announce that <b>Letzkhelo</b> is making a comeback! Mark your calendars for <b>March 31st</b>, as we are gearing up to host another electrifying Arm Wrestling event. Get ready to unleash your inner strength and compete against fellow athletes in a display of power and skill.</p>  
-      <p>Registration for the event will open on <b>March 25th</b>. Please note that spots are limited, and once registration closes, they will not be available. This is your chance to seize the opportunity and showcase your prowess on the arm wrestling stage.</p>  
+      <p>We are thrilled to announce that <b>Letzkhelo</b> is making a comeback! Mark your calendars for <b>May 5th</b>, as we are gearing up to host another electrifying Arm Wrestling event. Get ready to unleash your inner strength and compete against fellow athletes in a display of power and skill.</p>  
+      <p>Registration are open !!!. Please note that spots are limited, and once registration closes, they will not be available. This is your chance to seize the opportunity and showcase your prowess on the arm wrestling stage.</p>  
       <p>So, dust off those arm wrestling gloves, start training, and get prepared to give it your all. We are eagerly anticipating your participation and cannot wait to see you all there, ready to dominate the competition.</p>  
       <p>Stay tuned for further updates, and in the meantime, keep up the hard work and dedication to your craft.</p>  
       <p>Best regards,</p>  
       <p><b>Team Letzkhelo</b><br>  
-      +91 87438 37089, +91 87438 37089  </p>  
+      +91 87438 37089, +91 95605 50695 </p>  
     `};  
 
 
@@ -44,7 +44,7 @@ export async function GET(request: NextRequest) {
     try {
     //   const reqBody = await request.json();
        const users = await User.find();
-    // const users = [{email: "abhishek.sharma.letzkhelo@gmail.com", name: "Abhishek Sharma"}]
+    // const users = [{email: "sharmaash876@gmail.com", name: "Abhishek Sharma"}]
        console.log(users, "users")
 
        for (const user of users) {
--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -23,26 +23,46 @@ export const HomeComponent = () => {
   return (
     <div>
      
-      <div className="bg-gray-900">
+      <div className="bg-white ">
             <section className="container items-center px-4 pb-12 mx-auto  lg:flex md:px-40">
                 <div className="flex-1 space-y-4 sm:text-center lg:text-left">
-                    <h1 className="text-4xl font-bold  pt-5 text-yellow-500">
-                        Letzkhelo
+
+                  <div>
+                  <div>
+                  <h1 className="text-7xl font-bold  pt-5 text-black-500">
+                      Arm Wrestling
                     </h1>
-                    <p className="max-w-xl leading-relaxed text-gray-300 sm:mx-auto lg:ml-0">
-                       Come out of virtual world- Ab har gali se Champions niklenge
+                    <h1 className="text-7xl font-bold  pt-5 text-black-500">
+                      Platform
+                    </h1>
+                  </div>
+                    
+                    <div><h1 className="text-7xl font-bold  pt-5 text-yellow-500 underline" >
+                      LetzKhelo
+                    </h1></div>
+
+                  </div>
+
+                  
+                  <div className="max-w-xl font-bold  leading-relaxed text-black-700 sm:mx-auto lg:ml-0 text-2xl">" Ab Har Gali Se Champions Niklenge "</div>
+                  <h1 className="text-5xl font-bold  pt-5 text-black-500">
+                      <div className="text-5xl font-bold  pt-5 text-yellow-500">500+ </div>Registered Arm Wrestlers
+                    </h1>
+                    <p className="max-w-xl leading-relaxed text-gray-500 sm:mx-auto lg:ml-0">
                        <br/>
                        Our mission is simple: to create a vibrant and inclusive community for athletes of all levels. Whether you are a seasoned pro or just starting your sporting journey, Letzkhelo is the place where you can connect, compete, and grow.
                     </p>
-                    <div className="items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
+                    {/* <div className="items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
                     <Link href={'/allProducts'}
                             className="block px-6 py-2 text-center text-white bg-yellow-600 rounded-md"
                         >
                             Letzkhelo Store
                         </Link>
                       
-                    </div>
+                    </div> */}
+                    
                 </div>
+                
                 <div>
                     <img
                         src="/group.jpg"

1b74aef419a200193cb7b3b7d7d3d5065276c93f
abhi0504 <sharmaash876@gmail.com>
2024-05-12 19:18:41 +0530

    adding filtering

--- src/app/api/getAllEvents/route.tsx
+++ src/app/api/getAllEvents/route.tsx
@@ -1,20 +1,33 @@
-
-export const revalidate = 0;
-
 import { connect } from "@/dbConfig/dbConfig";
 import AddEvent from "@/models/events";
 import { NextRequest, NextResponse } from "next/server";
+import { parse } from 'url';
 
 connect();
 
 export async function GET(request: NextRequest) {
   try {
-    console.log("Hello")
-    const allEvents = await AddEvent.find();
-    return NextResponse.json(allEvents);
+    const { url } = request;
+    const { query } = parse(url || '', true);
+
+    const { date, entryFees, dateFilter, entryFeesFilter } = query;
+
+    // Prepare filter criteria
+    const filter: any = {};
+    if (date && dateFilter) {
+      filter.date = { [dateFilter as string]: date };
+    }
+    if (entryFees && entryFeesFilter) {
+      filter.entryFees = { [entryFeesFilter as string]: entryFees };
+    }
+
+    // Query events based on filter
+    const events = await AddEvent.find(filter);
+
+    return NextResponse.json(events);
   } catch (error: any) {
     return NextResponse.json(
-      { error: "Error fetching in All Events" },
+      { error: "Error fetching events" },
       { status: 500 }
     );
   }
